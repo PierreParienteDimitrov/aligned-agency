ccbabbddcbdde9bbe2dadc578f9a560c170887a8
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-03-30 15:49:54 +0200

    added carousel component

---
+++ components/carousel/CarouselItems.js
@@ -0,0 +1,26 @@
+export const CarouselItems = [
+	{
+		title: 'tempso',
+		description: '(re)discover classical music',
+		url: 'https://www.tempso.com/',
+		alt: 'tempso app screenshots',
+		cName: 'nav-links',
+		src: `/tempso.png`,
+	},
+	{
+		title: 'project2',
+		description: 'Lorem Ipsum',
+		url: 'https://www.tempso.com/',
+		alt: 'tempso app screenshots',
+		cName: 'nav-links',
+		src: `https://source.unsplash.com/random/1600x900`,
+	},
+	{
+		title: 'project3',
+		description: 'Lorem Ipsum',
+		url: 'https://www.tempso.com/',
+		alt: 'tempso app screenshots',
+		cName: 'nav-links',
+		src: `https://source.unsplash.com/WLUHO9A_xik/1600x900`,
+	},
+];
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 // import styles from '../styles/Home.module.css';
 
+import Carousel from '../components/carousel/Carousel';
 import AboutMe from '../components/homepage/AboutMe';
-import Grid from '../components/homepage/Grid';
+// import Grid from '../components/homepage/Grid';
 import { Header } from '../components/homepage/Header';
 
 export default function Home() {
 	return (
 		<div>
-			<Header />
-			<AboutMe />
-			<Grid />
+			<div className='grid grid-cols-1 md:grid-cols-2'>
+				<Header />
+				<AboutMe />
+			</div>
+			<Carousel />
+			{/* <Grid /> */}
 		</div>
 	);
 }
